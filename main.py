75807c5f8b9f59a6647e0bc9b4f9971a089294b1
Lorenzo Lodi <lodilorenzo@gmail.com>
2013-11-16 11:42:28 +0100

    First commit!

---
+++ main.py
@@ -0,0 +1,38 @@
+import os,sys
+os.environ['DJANGO_SETTINGS_MODULE'] = 'p2f-platform.settings'
+#
+## Force Django to reload its settings.
+from django.conf import settings
+settings._target = None
+
+import django.core.handlers.wsgi
+import django.core.signals
+import django.db
+import django.dispatch
+import webapp2
+
+# Log errors.
+#import logging
+#def log_exception(*args, **kwds):
+#    logging.exception('Exception in request:')
+#
+#django.dispatch.Signal.connect(
+#    django.core.signals.got_request_exception, log_exception)
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+# Unregister the rollback event handler.
+#
+#django.dispatch.Signal.disconnect(
+#    django.core.signals.got_request_exception,
+#    django.db._rollback_on_exception)
+#
+#app = django.core.handlers.wsgi.WSGIHandler()
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
